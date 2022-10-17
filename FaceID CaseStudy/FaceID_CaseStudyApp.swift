2becb50d6d616db7a29eb27d3f59fafe226d4a03
Jun <wnsgur4092@gmail.com>
2022-10-17 23:02:00 +1100

    Initial Commit

---
+++ FaceID CaseStudy/FaceID_CaseStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceID_CaseStudyApp.swift
+//  FaceID CaseStudy
+//
+//  Created by JunHyuk Lim on 17/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FaceID_CaseStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
